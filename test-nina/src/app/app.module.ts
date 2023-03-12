010fc4fdc7d1343d030961599e06c5db9d7693fa
Ana <beatriz9726@gmail.com>
2023-03-12 17:39:09 -0300

    :heavy_plus_sign: add complaint-list component

--- test-nina/src/app/app.module.ts
+++ test-nina/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComplaintListComponentComponent } from './complaint-list-component/complaint-list-component.component';
+import { ComplaintListComponent } from './complaint-list/complaint-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComplaintListComponentComponent,
+    ComplaintListComponent
   ],
   imports: [
     BrowserModule
---
+++ test-nina/src/app/complaint-list/complaint-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complaint-list',
+  templateUrl: './complaint-list.component.html',
+  styleUrls: ['./complaint-list.component.css']
+})
+export class ComplaintListComponent {
+
+}

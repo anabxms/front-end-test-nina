e0fe15b21211e8b51583d267b2afd0f2f8c85481
Ana <beatriz9726@gmail.com>
2023-03-13 08:49:49 -0300

    :heavy_plus_sign: try to add websocket and format document

--- test-nina/src/app/complaint-list/complaint-list.component.ts
+++ test-nina/src/app/complaint-list/complaint-list.component.ts
@@ -1,5 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { WebSocketSubject } from 'rxjs/webSocket';
+import { interval } from 'rxjs';
+
+const socket$ = new WebSocketSubject('ws://localhost:8080');
+
+socket$.subscribe(
+  (notification) => {
+    console.log('Conectado ao WebSocket');
+  },
+  (error) => {
+    console.error(error);
+  }
+);
+
+setInterval(() => {
+  socket$.next('Conectado ao WebSocket');
+}, 300000);
 
 interface ComplaintQueryParams {
   [key: string]: string | undefined;
@@ -11,19 +28,18 @@ interface ComplaintQueryParams {
 @Component({
   selector: 'app-complaint-list',
   templateUrl: './complaint-list.component.html',
-  styleUrls: ['./complaint-list.component.css']
+  styleUrls: ['./complaint-list.component.css'],
 })
 export class ComplaintListComponent implements OnInit {
-
   complaints!: any[];
   complaintPlaces!: string[];
   complaintTypes!: string[];
   complaintSituation!: string[];
-  selectedPlace = "";
-  selectedType = "";
-  selectedSituation = "";
+  selectedPlace = '';
+  selectedType = '';
+  selectedSituation = '';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getComplaints() {
     let params = new HttpParams();
@@ -36,15 +52,25 @@ export class ComplaintListComponent implements OnInit {
     if (this.selectedSituation) {
       params = params.append('situation', this.selectedSituation);
     }
-    this.http.get<any[]>('http://localhost:3000/complaints', { params: params })
-    .subscribe(data => {
-      this.complaints = data;
-      this.complaintPlaces = Array.from(new Set(data.map(complaint => complaint.place)));
-      this.complaintTypes = Array.from(new Set(data.map(complaint => JSON.stringify(complaint.type))))
-    .map(typeString => JSON.parse(typeString));
-      this.complaintSituation = Array.from(new Set(data.map(complaint => JSON.stringify(complaint.situation))))
-      .map(situationString => JSON.parse(situationString));
-    });
+    this.http
+      .get<any[]>('http://localhost:3000/complaints', { params: params })
+      .subscribe({
+        next: (data) => {
+          this.complaints = data;
+          this.complaintPlaces = Array.from(
+            new Set(data.map((complaint) => complaint.place))
+          );
+          this.complaintTypes = Array.from(
+            new Set(data.map((complaint) => JSON.stringify(complaint.type)))
+          ).map((typeString) => JSON.parse(typeString));
+          this.complaintSituation = Array.from(
+            new Set(
+              data.map((complaint) => JSON.stringify(complaint.situation))
+            )
+          ).map((situationString) => JSON.parse(situationString));
+        },
+        error: (error) => console.error(error),
+      });
   }
 
   filterComplaints() {
@@ -59,17 +85,18 @@ export class ComplaintListComponent implements OnInit {
       queryParams.situation = this.selectedSituation;
     }
     const queryString = Object.keys(queryParams)
-      .map(key => `${key}=${queryParams[key]}`)
+      .map((key) => `${key}=${queryParams[key]}`)
       .join('&');
     const url = `http://localhost:3000/complaints?${queryString}`;
-    this.http.get<any[]>(url)
-      .subscribe(data => {
+    this.http.get<any[]>(url).subscribe({
+      next: (data) => {
         this.complaints = data;
-      });
+      },
+      error: (error) => console.error(error),
+    });
   }
 
   ngOnInit() {
     this.getComplaints();
   }
-
 }
